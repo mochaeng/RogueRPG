46c98b442710f18a6008c1907718a8f87ae5740f
mochaeng <campos.antoonio@gmail.com>
2022-10-14 17:22:05 -0300

    hurtboxes

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,13 +1,22 @@
 extends KinematicBody2D
+class_name Player
 
 var ACCELERATION: int = 500
 var MAX_SPEED: int = 80
 var FRICTION: int = 500
 
+enum {
+	MOVE,
+	ATTACK
+}
+
 var velocity: Vector2 = Vector2.ZERO
+var current_state = MOVE
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
 onready var sprite: Sprite = $Sprite
+onready var sword_hitbox_collision: CollisionShape2D = $SwordHitBox/CollisionShape2D
+
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,6 +24,14 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	match current_state:
+		MOVE:
+			move(delta, input_vector)
+		ATTACK:
+			attack(input_vector)
+
+
+func move(delta, input_vector: Vector2) -> void:
 	if input_vector != Vector2.ZERO:
 		_split_sprite(input_vector)
 		animation_player.play("move")
@@ -22,11 +39,25 @@ func _physics_process(delta):
 	else:
 		animation_player.play("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_pressed("ui_attack"):
+		current_state = ATTACK
+
+func attack(input_vector: Vector2) -> void:
+	velocity = Vector2.ZERO
+	_split_sprite(input_vector)
+	animation_player.play("attack")
+
 
 func _split_sprite(input_vector: Vector2):
 	if input_vector.x > 0:
 		sprite.flip_h = false
-	else:
-		sprite.flip_h = true	
+		sword_hitbox_collision.position = Vector2(10, 0)
+	elif input_vector.x < 0:
+		sprite.flip_h = true
+		sword_hitbox_collision.position = Vector2(-10, 0)
+
+
+func attack_finished() -> void:
+	current_state = MOVE
